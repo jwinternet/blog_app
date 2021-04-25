5bd0ba941c2fe0efd486dbc95e35368440e08c63
jwinternet <47834941+Killface0@users.noreply.github.com>
2021-04-25 19:48:08 -0400

    Finished chapter 2.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     # post views
-    # path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/',
@@ -17,5 +17,10 @@ urlpatterns = [
         '<int:post_id>/share/',
         views.post_share, 
         name='post_share'
+        ),
+    path(
+        'tag/<slug:tag_slug>/',
+        views.post_list, 
+        name='post_list_by_tag'
         ),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,8 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -13,8 +15,14 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -28,7 +36,7 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'page': page, 'posts': posts}
+        {'page': page, 'posts': posts, 'tag': tag}
     )
 
 
@@ -56,6 +64,16 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+        ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+        ).order_by('-same_tags','-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
@@ -63,7 +81,8 @@ def post_detail(request, year, month, day, post):
             'post': post,
             'comments': comments,
             'new_comment': new_comment,
-            'comment_form': comment_form
+            'comment_form': comment_form,
+            'similar_posts': similar_posts
         }
         )
 
